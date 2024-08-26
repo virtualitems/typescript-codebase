41cb6b443229d2ac9b0af0a2f189d9fe92a6e12f
Alejandro CR <contacto@alejandrocr.co>
2024-08-26 02:00:54 -0500

    create base.ejs and add ejs configurations to main.ts

--- source/main.ts
+++ source/main.ts
@@ -2,11 +2,20 @@
  * @fileoverview Typescript REST API.
  */
 
+// Libraries
+
 import express from 'express';
+
+// Constants
 
 const port = 3000;
 
+// Express
+
 const app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', './source/views');
 
 app.use(express.static('public'));
 
