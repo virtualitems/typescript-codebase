1727c279334bf694e7ff10d4bf614a060e4f9651
Alejandro CR <contacto@alejandrocr.co>
2024-08-26 01:34:52 -0500

    add express adapter

--- source/server.ts
+++ source/server.ts
@@ -1,13 +1,69 @@
-/**
- * @fileoverview REST API with Express and TypeScript
- */
+// Libraries
 
 import express from 'express';
 
-const port = 3000;
+// Shared Module
 
-const app = express();
+// Other Modules
 
-app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
-});
+// Same Layer
+
+// Lower Layers
+
+// Types
+
+// Interfaces
+
+// Constants
+
+
+/**
+ * @description Express application adapter.
+ */
+export default class Server
+{
+
+    [property: string]: unknown;
+
+    // public ATTRIBUTES
+
+    public port;
+
+    // protected ATTRIBUTES
+
+    protected app: express.Application;
+
+    // private ATTRIBUTES
+
+    // public static ATTRIBUTES
+
+    // protected static ATTRIBUTES
+
+    // private static ATTRIBUTES
+
+    // Constructor, Getters, Setters
+
+    public constructor(port: number)
+    {
+        this.port = port;
+        this.app = express();
+    }
+
+    // public METHODS
+
+    public listen(callback: () => void): void
+    {
+        this.app.listen(this.port, callback);
+    }
+
+    // protected METHODS
+
+    // private METHODS
+
+    // public static METHODS
+
+    // protected static METHODS
+
+    // private static METHODS
+
+} //:: class
