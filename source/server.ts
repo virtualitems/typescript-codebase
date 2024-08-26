d47b54630676749bfc7a018890b8c0571f295c5c
Alejandro CR <contacto@alejandrocr.co>
2024-08-26 01:27:35 -0500

    create server.ts

---
+++ source/server.ts
@@ -0,0 +1,13 @@
+/**
+ * @fileoverview REST API with Express and TypeScript
+ */
+
+import express from 'express';
+
+const port = 3000;
+
+const app = express();
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
