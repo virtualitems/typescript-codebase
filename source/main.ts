01e03a095b7f6a473f85e29842c945d78e944f02
Alejandro CR <contacto@alejandrocr.co>
2024-08-26 01:42:28 -0500

    transform project architecture to mvc

--- source/main.ts
+++ source/main.ts
@@ -2,4 +2,12 @@
  * @fileoverview This file is used only for exporting ports.
  */
 
-throw new Error('Not implemented yet');
+import express from 'express';
+
+const port = 3000;
+
+const app = express();
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
