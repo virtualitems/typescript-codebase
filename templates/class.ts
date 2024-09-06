2a69af1fe1005eba5d31ccbbf5a4074d731d6a87
Alejandro CR <contacto@alejandrocr.co>
2024-09-06 15:37:25 -0500

    refactor template dir files

---
+++ templates/class.ts
@@ -0,0 +1,50 @@
+// Libraries
+
+// Shared Module
+
+// Other Modules
+
+// Same Layer
+
+// Lower Layers
+
+// Types
+
+// Constants
+
+
+/**
+ * @description 
+ */
+export default class __CLASS__ // extends // declarations first // overrides second // abstract last
+{
+
+    [property: string]: unknown;
+
+    // public ATTRIBUTES
+
+    // protected ATTRIBUTES
+
+    // private ATTRIBUTES
+
+    // public static ATTRIBUTES
+
+    // protected static ATTRIBUTES
+
+    // private static ATTRIBUTES
+
+    // Constructor, Getters, Setters
+
+    // public METHODS
+
+    // protected METHODS
+
+    // private METHODS
+
+    // public static METHODS
+
+    // protected static METHODS
+
+    // private static METHODS
+
+} //:: class
