6345adedd99558f5aab7baf645ef08482ff0f7fc
Alejandro CR <contacto@alejandrocr.co>
2024-09-06 00:19:39 -0500

    restore shared adapters

---
+++ source/shared/adapters/data/ArrayDataManager.ts
@@ -0,0 +1,71 @@
+// Libraries
+
+// Same Layer
+
+// Lower Layers
+
+import DataManager from '../../application/data/DataManager.js';
+
+// Types
+
+// Interfaces
+
+// Constants
+
+
+/**
+* @description 
+*/
+export default class ArrayDataManager<T extends TObject> extends DataManager
+{
+
+    [property: string | symbol]: unknown;
+
+    // Public Attributes
+
+    // Protected Attributes
+
+    protected _connection: T[] | null;
+
+    protected _database: T[];
+
+    // Private Attributes
+
+    // Public Static Attributes
+
+    // Protected Static Attributes
+
+    // Private Static Attributes
+
+    // Constructor, Getters, Setters
+
+    constructor(database: T[])
+    {
+        super();
+        this._database = database;
+        this._connection = null;
+    }
+
+    // Public Methods
+
+    public override async connect(): Promise<void>
+    {
+        this._connection = this._database;  // Set the connection
+    }
+
+    public override async disconnect(): Promise<void>
+    {
+        this._connection = null;  // Clear the connection
+    }
+
+    // Protected Methods
+
+    // Private Methods
+
+    // Public Static Methods
+
+    // Protected Static Methods
+
+    // Private Static Methods
+
+} //:: class
---
+++ source/shared/adapters/events/InMemoryEventDispatcher.ts
@@ -0,0 +1,104 @@
+// Libraries
+
+// Same Layer
+
+// Lower Layers
+
+import type Event from '../../application/events/Event.js';
+import type EventHandler from '../../application/events/EventHandler.js';
+
+import EventDispatcher from '../../application/events/EventDispatcher.js';
+
+// Types
+
+// Interfaces
+
+// Constants
+
+
+/**
+* @description 
+*/
+export default class InMemoryEventDispatcher extends EventDispatcher
+{
+
+    [property: string | symbol]: unknown;
+
+    // public ATTRIBUTES
+
+    // protected ATTRIBUTES
+
+    protected readonly _observers: Map<Function, Set<EventHandler>>;
+
+    // private ATTRIBUTES
+
+    // public static ATTRIBUTES
+
+    // protected static ATTRIBUTES
+
+    // private static ATTRIBUTES
+
+    // Constructor, Getters, Setters
+
+    public constructor()
+    {
+        super();
+        this._observers = new Map<Function, Set<EventHandler>>();
+    }
+
+    // public METHODS
+
+    public subscribe(constructor: Function, handler: EventHandler): void
+    {
+
+        const list = this._observers.get(constructor);
+
+        if (list === undefined) {
+            this._observers.set(constructor, new Set([handler]));
+            return;
+        }
+
+        list.add(handler);
+    }
+
+    public unsubscribe(constructor: Function, handler: EventHandler): void
+    {
+        const list = this._observers.get(constructor);
+
+        if (list === undefined) {
+            return;
+        }
+
+        list.delete(handler);
+
+        if (list.size === 0) {
+            this._observers.delete(constructor);
+        }
+
+    }
+
+    public dispatch(event: Event): void
+    {
+        const list = this._observers.get(event.constructor);
+
+        if (list === undefined) {
+            return;
+        }
+
+        for (const handler of list) {
+            handler.handle(event);
+        }
+
+    }
+
+    // protected METHODS
+
+    // private METHODS
+
+    // public static METHODS
+
+    // protected static METHODS
+
+    // private static METHODS
+
+} //:: class
