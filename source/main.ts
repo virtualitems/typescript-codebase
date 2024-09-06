aae308ddfc4b735504e65656d64e0b6e2c7e4477
Alejandro CR <contacto@alejandrocr.co>
2024-09-06 17:48:53 -0500

    update index-d and main.ts

--- source/main.ts
+++ source/main.ts
@@ -1,5 +1,5 @@
 /**
- * @fileoverview This file is used only for exporting ports.
+ * @fileoverview This file is used only for exporting the main components of the library.
  */
 
 throw new Error('Not implemented yet');
