7b0d9738d88509abcbfebb1a0b61297316c2ac8b
Alejandro CR <contacto@alejandrocr.co>
2024-09-15 01:00:27 -0500

    Rename Facade to Service in application layer and Service to Aggregate in domain layer

--- templates/lib/shared/application/Service.ts
+++ templates/lib/shared/application/Service.ts
@@ -10,9 +10,8 @@
 
 
 /**
- * @description A service is a class that contains business logic that doesn't belong to any entity.
- * It is used to perform operations that don't fit into an entity or involve multiple entities.
- * Normally, method arguments are entities.
+ * @description 
+ * function arguments are Entity
  */
 export default abstract class Service
 {
--- templates/lib/shared/domain/Aggregate.ts
+++ templates/lib/shared/domain/Aggregate.ts
@@ -10,10 +10,11 @@
 
 
 /**
- * @description 
- * function arguments are Entity
+ * @description An Aggregate is a class that contains business logic that doesn't belong to any entity.
+ * It is used to perform operations that don't fit into an entity or involve multiple entities.
+ * Normally, method arguments are entities.
  */
-export default abstract class Facade
+export default abstract class Aggregate
 {
 
     [property: string]: unknown;
