864a28afc3acea5c3baee67a4f8dffb81a087772
Alejandro CR <contacto@alejandrocr.co>
2024-09-15 01:36:11 -0500

    update Event.ts

--- templates/lib/shared/application/events/Event.ts
+++ templates/lib/shared/application/events/Event.ts
@@ -21,7 +21,7 @@ export default abstract class Event
 
     public readonly timestamp: number;
 
-    public readonly details?: Record<string, unknown>;
+    public readonly details: Record<string, unknown>;
 
     // protected ATTRIBUTES
 
@@ -38,7 +38,7 @@ export default abstract class Event
     public constructor(timestamp: number, details?: Record<string, unknown>)
     {
         this.timestamp = timestamp;
-        this.details = details;
+        this.details = details ?? {};
     }
 
     // public METHODS
