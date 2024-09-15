8275a42b62f8dc410d432c2a9a9ae972cadc4f4e
Alejandro CR <contacto@alejandrocr.co>
2024-09-15 00:48:17 -0500

    Rename Service.ts to Aggregate.ts

--- source/shared/domain/Aggregate.ts
+++ source/shared/domain/Aggregate.ts
@@ -10,11 +10,11 @@
 
 
 /**
- * @description A service is a class that contains business logic that doesn't belong to any entity.
+ * @description An Aggregate is a class that contains business logic that doesn't belong to any entity.
  * It is used to perform operations that don't fit into an entity or involve multiple entities.
  * Normally, method arguments are entities.
  */
-export default abstract class Service
+export default abstract class Aggregate
 {
 
     [property: string]: unknown;
